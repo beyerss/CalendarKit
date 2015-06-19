c5ee45202395fa85c40a0745f4381f61781327ec
Steven Beyers <sbeyers@beyersapps.com>
2015-06-18 22:29:04 -0400

    Update the test project to make use of the delegate methods.

--- CalendarKitTestApp/CalendarKitTestApp/ViewController.swift
+++ CalendarKitTestApp/CalendarKitTestApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CalendarKit
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let calendar = segue.destinationViewController as? Calendar {
+            calendar.delegate = self
+            
+            // tell the calendar to start with tomorrow selected
+            calendar.selectedDate = NSDate().dateByAddingTimeInterval(60*60*24)
+        }
+    }
 
 }
 
+extension ViewController: CalendarDelegate {
+    
+    func calendar(calendar: Calendar, didSelectDate date: NSDate) {
+        print("Current selected date: \(date)")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func calendar(calendar: Calendar, didScrollToDate date: NSDate, withNumberOfWeeks weeks: Int) {
+        print("Scrolled to date (\(date)) with \(weeks) weeks")
+    }
+    
+}
